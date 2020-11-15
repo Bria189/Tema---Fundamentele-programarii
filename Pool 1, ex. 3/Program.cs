782ce5e9727cdb36e9cae151b02691e4af93f9bf
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-11-15 16:13:27 +0200

    Rezolvare ex 3

---
+++ Pool 1, ex. 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Pool_1__ex._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determinati daca n se divide cu k, unde n si k sunt date de intrare. 
+
+            int n, k;
+
+            n = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+
+            if (n % k == 0)
+                Console.Write("n se divide cu k");
+            else
+                Console.Write("n NU se divide cu k");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
