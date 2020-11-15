be81ef66ee1e29c04d67fad93ab3bb8260743a98
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-11-15 16:52:14 +0200

    Rezolvare ex 7

---
+++ Pool 1, ex 7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pool_1__ex_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor. 
+            int a, b, aux;
+            Console.WriteLine("Introduceti o valoare");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti alta valoare");
+            b = int.Parse(Console.ReadLine());
+           
+            aux = a;
+                a = b;
+                b = aux;
+
+            Console.WriteLine("Dupa interschimbare valorile sunt:");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
