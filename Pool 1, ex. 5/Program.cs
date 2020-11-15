dc246a33e10ac5ee4c1b1fc0ac287b9098778e02
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-11-15 16:38:11 +0200

    Rezolvare ex 5

---
+++ Pool 1, ex. 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Pool_1__ex._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Extrageti si afisati a k-a cifra de la sfarsitul unui numar. Cifrele se numara de la dreapta la stanga. 
+
+            int k, nr, n;
+            Console.WriteLine("Introduceti o valoare pentru k");
+            k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti o valoare pentru n");
+            n = int.Parse(Console.ReadLine());
+
+            nr = 0; //numaram cate cifre am eliminat din numarul n
+
+            while (n != 0)
+                if ((k - nr) != 1)
+                {
+                    n = n / 10;
+                    nr++;
+                }
+                else
+                {
+                    Console.Write(n % 10);
+                    break;
+                }
+
+                        
+        }
+    }
+}
