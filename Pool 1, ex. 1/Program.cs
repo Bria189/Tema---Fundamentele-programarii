47b59f5c55e62cb66322af4372dc17d9918ee438
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-11-15 15:30:32 +0200

    Rezolvare ex 1

---
+++ Pool 1, ex. 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Pool_1__ex._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rezolvati ecuatia de gradul 1 cu o necunoscuta: ax+b = 0, unde a si b sunt date de intrare.
+            int a, b;
+            float x;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            x = -b / a;
+
+            Console.Write(x);
+            Console.ReadKey();
+
+
+        }
+    }
+}
