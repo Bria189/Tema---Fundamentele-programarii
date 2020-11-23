37567d31e1791a1cb379505c0a7aa6e8da8b09ab
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-11-23 19:02:49 +0200

    Rezolvare ex 9

---
+++ Pool 1, ex 9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pool_1__ex_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Afisati toti divizorii numarului n. 
+            int n, i;
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Divizorii numarului sunt:");
+            for (i = 1; i <= n; i++)
+                if (n % i == 0)
+                    Console.Write(i + " ");
+        }
+    }
+}
