8981098f49bf29be8c84d4dcdf4cade201947f31
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-11-23 19:05:49 +0200

    Rezolvare ex 10

---
+++ Pool 1, ex 10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pool_1__ex_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test de primalitate: determinati daca un numar n este prim.
+            int n, i, nr = 0;
+            n = int.Parse(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+                if (n % i == 0)
+                    nr++;
+            if (nr == 2)
+                Console.Write("Numarul este prim");
+            else
+                Console.Write("Numarul nu este prim");
+
+        }
+    }
+}
