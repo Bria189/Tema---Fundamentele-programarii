108808843e79d751374beab756ef6ccf6ba65b88
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-11-23 19:19:41 +0200

    Rezolvare ex 14

---
+++ Pool 1, ex 14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pool_1__ex_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determianti daca un numar n este palindrom. (un numar este palindrom daca citit invers obtinem un numar egal cu el, de ex. 121 sau 12321. 
+            int n, m, ogl=0;
+            n = int.Parse(Console.ReadLine());
+            m = n;
+
+            while(m!=0)
+            {
+                ogl = ogl * 10 + m % 10;
+                m = m / 10;
+            }
+            if (ogl == n)
+                Console.WriteLine("Numarul este palidrom");
+            else
+                Console.WriteLine("Numarul nu este palidrom");
+        }
+    }
+}
