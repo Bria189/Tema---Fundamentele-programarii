1860ecbcc4f4db98066b726f8797459e0d3df6d7
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-11-23 19:45:55 +0200

    Rezolvare ex 21

---
+++ Pool 1, ex 21/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Pool_1__ex_21
+{
+    class Program
+    {
+        static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            //Ghiciti un numar intre 1 si 1024 prin intrebari de forma "numarul este mai mare sau egal decat x?".
+            int n, x;
+            n = rnd.Next(1024);
+            Console.WriteLine("Introduceti numarul:");
+            x = int.Parse(Console.ReadLine());
+            if (x == n)
+                Console.WriteLine("Ai ghicit numarul! Felicitari");
+            while (x != n)
+            {
+                if (x < n)
+                {
+                    Console.WriteLine("Numarul este prea mic. Mai incearca.");
+                    x = int.Parse(Console.ReadLine());
+                    if(x==n)
+                        Console.WriteLine("Ai ghicit numarul! Felicitari");
+                }
+                if (x > n)
+                {
+                    Console.WriteLine("Numarul este prea mare. Mai incearca.");
+                    x = int.Parse(Console.ReadLine());
+                    if (x == n)
+                        Console.WriteLine("Ai ghicit numarul! Felicitari");
+                }
+            }
+
+
+        }
+    }
+}
