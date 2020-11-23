02bfaa5c86f2ef7b2ac3fad8d1edd45572f083d2
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-11-23 19:14:54 +0200

    Rezolvare ex 12

---
+++ Pool 1, ex 12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pool_1__ex_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determinati cate numere intregi divizibile cu n se afla in intervalul [a, b]
+            int n, a, b, nr=0, aux;
+            n = int.Parse(Console.ReadLine());
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            if (b < a)
+            {   aux = a;
+                a = b;
+                b = a;
+            }
+            for (int i = a; i <= b; i++)
+                if (i % n == 0)
+                    nr++;
+            Console.WriteLine(nr);
+        }
+    }
+}
