b4a2671d5e4ee17ad9283916873afa68899ced1f
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-11-23 18:59:07 +0200

    Rezolvare ex 8

---
+++ Ex lab incep 2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Ex_lab_incep_2
+{
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            //Afisati cel mai mare si cel mai mic numar format cu cifrele unui numar dat
+            int[] v;
+            int n=100, k, i;
+            v = new int[n];
+            k = int.Parse(Console.ReadLine());
+            i = 0;
+
+            while(k!=0)
+            {
+                v[i] = k % 10;
+                k = k / 10;
+                i++;
+                Console.WriteLine(v[i]);
+
+            }
+            n = i - 1;
+            int aux, j, nr=0;
+            for (i = 0; i < n - 1; i++)
+                for(j=i+1; j<n; j++)
+                {
+                    if(v[i]<v[j])
+                    {
+                        aux = v[i];
+                        v[i] = v[j];
+                        v[j] = aux;
+                    }
+                }
+            for (i = 0; i < n; i++)
+                Console.Write(v[i] + " ");
+
+            for(i=0; i< n; i++)
+            {
+                Console.Write(v[i]);
+                
+            }
+            Console.WriteLine();
+            for (i = n-1; i > 0; i--)
+            {
+                if (v[i] != 0)
+                {
+                    Console.Write(v[i]);
+                    nr = 0;
+                }
+                else
+                    nr = 1;
+                
+                if (nr==1)
+                    Console.Write(nr-1);
+                
+            }
+            
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ Ex lab incep/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Ex_lab_incep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se da un vector si se cere suma celor mai mari 3 valori
+            Console.WriteLine("Introduceti o valoare pentru n si mai apoi valorile din vector");
+            int n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            int max1, max2, max3, i, s = 0, aux;
+            
+            
+            for (i = 0; i < n; i++)
+               v[i]=int.Parse( Console.ReadLine());
+
+            Console.WriteLine("Maximele initiale sunt:");
+            max1 = v[0];
+            max2 = v[1];
+            max3 = v[2];
+            do
+            {
+                if (max1 > max2)
+                {
+                    aux = max2;
+                    max2 = max1;
+                    max1 = aux;
+                }
+                else
+                    if (max1 > max3)
+                {
+                    aux = max3;
+                    max3 = max1;
+                    max1 = aux;
+                }
+                else
+                    if (max2 > max3)
+                {
+                    aux = max3;
+                    max3 = max2;
+                    max2 = aux;
+                }
+                Console.WriteLine(max1);
+                Console.WriteLine(max2);
+                Console.WriteLine(max3);
+            } while (max1 > max2 || max2 > max3);
+
+
+            for (i = 3; i < n; i++)
+            {
+                if (v[i] > max3)
+                {
+                    aux = max3;
+                    max3 = v[i];
+                    Console.WriteLine("Noile maxime sunt:");
+                    Console.WriteLine(max1);
+                    Console.WriteLine(max2);
+                    Console.WriteLine(max3);
+                    int aux2 = max2;
+                    max2 = aux;
+                    max1 = aux2;
+                }
+                else
+                 if (v[i] > max2)
+                {
+                    aux = max2;
+                    max2 = v[i];
+                    Console.WriteLine("Noile maxime sunt:");
+                    Console.WriteLine(max1);
+                    Console.WriteLine(max2);
+                    Console.WriteLine(max3);
+                    max1 = aux;
+                }
+                else
+                 if (v[i] > max1)
+                {
+                    max1 = v[i];
+                    Console.WriteLine("Noile maxime sunt:");
+                    Console.WriteLine(max1);
+                    Console.WriteLine(max2);
+                    Console.WriteLine(max3);
+                }
+                }
+            s = max1 + max2 + max3;
+            Console.WriteLine("Suma celor mai mari 3 valori din vector este:");
+            Console.WriteLine(s);
+            Console.ReadKey();
+            }
+    }
+}
