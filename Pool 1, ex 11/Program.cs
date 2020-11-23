ac87968de521c3a3dd6a6830b2e56e59427b953b
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-11-23 19:08:15 +0200

    Rezolvare ex 11

---
+++ Pool 1, ex 11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pool_1__ex_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Afisati in ordine inversa cifrele unui numar n. 
+            int n;
+            n = int.Parse(Console.ReadLine());
+            while(n!=0)
+            {
+                Console.Write(n % 10 + " ");
+                n = n / 10;
+            }    
+        }
+    }
+}
