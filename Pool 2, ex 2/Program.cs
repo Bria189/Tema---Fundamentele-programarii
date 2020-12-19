8dc0e0dfa536a783897938f58b9e1bbba1871d8b
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-12-19 18:41:20 +0200

    Rezolvare ex 2

---
+++ Pool 2, ex 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Pool_2__ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se da o secventa de n numere. Sa se determine cate sunt negative, cate sunt zero si cate sunt pozitive
+
+            int n, x, poz = 0, neg = 0, nule = 0;
+
+            Console.WriteLine("Introduceti o valoare pentru n");
+            n = int.Parse(Console.ReadLine());
+
+            while (n != 0)
+            {
+                Console.WriteLine("Introduceti o valoare");
+                x = int.Parse(Console.ReadLine());
+                if (x < 0)
+                    neg++;
+                else
+                    if (x > 0)
+                    poz++;
+                else
+                    nule++;
+                n--;
+            }
+            Console.WriteLine(neg + " numere negative");
+            Console.WriteLine(poz + " numere pozitive");
+            Console.WriteLine(nule + " numere nule");
+            Console.ReadKey();
+
+        }
+    }
+}
