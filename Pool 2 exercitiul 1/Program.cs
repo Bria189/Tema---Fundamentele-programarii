c4f3e4f43f2dc0e0e53b1767921091646a08c796
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-12-19 18:32:11 +0200

    Rezolvare ex 1

---
+++ Pool 2 exercitiul 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Pool_2_exercitiul_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se da o secventa de n numere. Sa se determine cate din ele sunt pare.
+
+            int n, x, nr = 0;
+
+            Console.WriteLine("Introduceti o valoare pentru n");
+
+            n = int.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                Console.WriteLine("Introduceti o valoare");
+                x = int.Parse(Console.ReadLine());
+                if (x % 2 == 0)
+                    nr++;
+                n--;
+
+            }
+            Console.WriteLine(nr + "numere sunt pare");
+            Console.ReadKey();
+        }
+    }
+}
