b8ba44a61dca500ca5f04979fb6ac618dd1ee9a6
Brianna Durdeu <brianna_durdeu@yahoo.com>
2020-12-19 18:44:42 +0200

    Rezolvare ex 3

---
+++ Pool 2, ex 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Pool_2__ex_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculati suma si produsul numerelor de la 1 la n. 
+
+            Console.WriteLine("Introduceti o valoare pentru n");
+
+            int n, s = 0, p = 1, i;
+            n = int.Parse(Console.ReadLine());
+            for(i=1; i<=n; i++)
+            {
+                s = s + i;
+                p = p * i;
+
+            }
+            Console.WriteLine("Suma numerelor este = " + s);
+            Console.WriteLine("Produsul numerelor este = " + p);
+            Console.ReadKey();
+        }
+    }
+}
